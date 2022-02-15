7a02695f9d9b8dec7dc534a4fe04d3a076e3aa19
gasco <stefano.gasco@copebit.ch>
2022-02-15 10:16:23 +0100

    improved regex

--- pre-commit-hook.ps1
+++ pre-commit-hook.ps1
@@ -1,9 +1,8 @@
 #!/usr/bin/env pwsh
 
 $TOT = 19
-$readme = Get-Content README.md
+$fileName = 'README.md'
 $advancement_count = (Get-ChildItem -Directory).Count
 $advancement_perc = [math]::Round(($advancement_count*100)/$TOT)
-$res_readme = $readme -replace '[0-9]+', $advancement_perc
-Out-File -FilePath .\README.md -InputObject $res_readme -Encoding ASCII
+(Get-Content -Path $fileName -Raw) -replace '(https://progress-bar.dev)/[0-9]+', "`$1/$advancement_perc" | Set-Content -Path $fileName -noNewline
 git add .\README.md
--- test.ps1
+++
@@ -1,5 +0,0 @@
-$a = 19  
-$b = 100
-$d = 2  
-$c = [math]::Round(($d*$b)/$a)
-Write-Host $c
